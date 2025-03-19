47bbcbeea3068af415ebbe05d3f0529c56c13ea9
Petro Smarkutskyi <s31026@pjwstk.edu.pl>
2025-03-19 16:42:53 +0100

    Zakonczenie klasy Kontener

--- Cwicz3/Cwicz3/Kontener.cs
+++ Cwicz3/Cwicz3/Kontener.cs
@@ -1,6 +1,33 @@
 namespace Cwicz3;
 
-public class Kontener
+public abstract class Kontener
 {
+    private double _masaLadunku { get; set; }
+    private double _wysokosc { get; set; }
+    private double _wagaWlasna { get; set; }
+    private double _glebokosc { get; set; }
+    private String _numerSeryjny { get; set; }
+    private double _maksymalnaLadownosc { get; set; }
+
+
+    public void oproznienieLadunku()
+    {
+        _masaLadunku = 0;
+    }
+
+    public void zaladowanieLadunku(double masaLadunku)
+    {
+        if (masaLadunku > _maksymalnaLadownosc)
+        {
+            //throw new OverfillException();
+        }
+        
+        this._masaLadunku = masaLadunku;
+        
+    }
+    
+    
+    
+    
     
 }

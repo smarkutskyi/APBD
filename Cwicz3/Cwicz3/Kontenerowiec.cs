0e87d6b57001859de9d6fda7a042e63cd23f560c
Piotr Smarkutskyi <spospo010@gmail.com>
2025-03-25 22:51:44 +0100

    Zrobiona klasa Kontenerowiec

---
+++ Cwicz3/Cwicz3/Kontenerowiec.cs
@@ -0,0 +1,19 @@
+namespace Cwicz3;
+
+public class Kontenerowiec
+{
+    private List<Kontener> kontener { get; }
+    
+    private double maksPredkosc { get;}
+    private int liczbaKontenerow { get; }
+    private double wagaSumyKontenerow { get; }
+
+    public Kontenerowiec(List<Kontener> kontener, double maksPredkosc, int liczbaKontenerow, double wagaSumyKontenerow)
+    {
+        this.kontener = kontener;
+        this.maksPredkosc = maksPredkosc;
+        this.liczbaKontenerow = liczbaKontenerow;
+        this.wagaSumyKontenerow = wagaSumyKontenerow;
+    }
+    
+}

8d7988d01e56e0729f41479b52241f87b8aa759c
Piotr Smarkutskyi <spospo010@gmail.com>
2025-04-07 21:03:34 +0200

    Rozwiązania problemu z ide i wykonano 3 pytania

--- Tutorial4/Tutorial4Tests/EmpDeptSalgradeTests.cs
+++ Tutorial4/Tutorial4Tests/EmpDeptSalgradeTests.cs
@@ -9,7 +9,7 @@ public class EmpDeptSalgradeTests
     {
         var emps = Database.GetEmps();
 
-        List<Emp> result = null; 
+        List<Emp> result = emps.Where(e => e.Job == "SALESMAN").ToList(); 
 
         Assert.Equal(2, result.Count);
         Assert.All(result, e => Assert.Equal("SALESMAN", e.Job));
@@ -22,7 +22,9 @@ public class EmpDeptSalgradeTests
     {
         var emps = Database.GetEmps();
 
-        List<Emp> result = null; 
+        List<Emp> result = emps.Where(e => e.DeptNo == 30).OrderByDescending(e => e.Sal).ToList();
+        
+        
 
         Assert.Equal(2, result.Count);
         Assert.True(result[0].Sal >= result[1].Sal);
@@ -36,7 +38,12 @@ public class EmpDeptSalgradeTests
         var emps = Database.GetEmps();
         var depts = Database.GetDepts();
 
-        List<Emp> result = null; 
+        var deptNo = depts
+            .Where(d => d.Loc == "CHICAGO")
+            .Select(d => d.DeptNo)
+            .ToList();
+        
+        List<Emp> result = emps.Where(e => deptNo.Contains(e.DeptNo)).ToList();  
 
         Assert.All(result, e => Assert.Equal(30, e.DeptNo));
     }
